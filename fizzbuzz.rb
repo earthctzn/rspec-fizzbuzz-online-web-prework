7dbf400e3b8ab8368798ca94b9bfbc83ded41c3a
Caleb Aviles <earthctzn1@gmail.com>
2019-04-02 05:48:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,8 @@ def fizzbuzz(int)
     return "Fizz"
     elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 3 and int % 5 == 3
+  if int % 3 == 0 
+    if int % 5 == 0
   return "FizzBuzz"
   end
 end# Don't forget! This file needs to be 'required' in its spec file
